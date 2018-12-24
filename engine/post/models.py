fd260a49726f63620066baf10247527f28a9ed37
Giovanni <giovanniyatze@gmail.com>
2018-12-24 00:50:41 -0600

    type in post model fixed

--- engine/post/models.py
+++ engine/post/models.py
@@ -10,13 +10,13 @@ class Post(models.Model):
     name = models.CharField(max_length=254, blank=False)
     content = models.TextField(blank=False)
     category = models.ForeignKey(Category, related_name='post_category',\
-        null=True, editable=False, on_delete=models.SET_NUL)
+        null=True, editable=False, on_delete=models.SET_NULL)
     subcategories = models.ForeignKey(Subcategory, related_name='post_subcategory',\
-        null=True, editable=False, on_delete=models.SET_NUL)
+        null=True, editable=False, on_delete=models.SET_NULL)
     hashtags = models.ForeignKey(Hashtag, related_name='post_hashtags',\
-        null=True, editable=False, on_delete=models.SET_NUL)
+        null=True, editable=False, on_delete=models.SET_NULL)
     created_by = models.ForeignKey(User, related_name='user_post',\
-        null=True, editable=False, on_delete=models.SET_NUL)
+        null=True, editable=False, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(default=now, editable=False)
     changed = models.DateTimeField(default=now, editable=True)
     active = models.BooleanField(default=False)
