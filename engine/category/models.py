bfebe0d4b8edfbfb717d51680700921d78e64249
Giovanni <giovanniyatze@gmail.com>
2018-12-24 00:17:36 -0600

    Subcategory model added

--- engine/category/models.py
+++ engine/category/models.py
@@ -3,10 +3,24 @@ from django.utils import timezone
 
 from engine.account.models import User
 
+
 class Category(models.Model):
-    name = models.CharField(blank=False, null=False, default='')
+    name = models.CharField(max_length=254, blank=False, null=False, default='')
     description = models.TextField(blank=False, null=True, default='')
     created_by = models.ForeignKey(User, related_name='user_category',\
+        null=True, editable=False, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(default=timezone.now(), editable=True)
+    changed = models.DateTimeField(default=timezone.now(), editable=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('created_at',)
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=254, blank=False, null=False, default='')
+    description = models.TextField(blank=False, null=True, default='')
+    created_by = models.ForeignKey(User, related_name='user_subcategory',\
         null=True, editable=False, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(default=timezone.now(), editable=True)
     changed = models.DateTimeField(default=timezone.now(), editable=True)
