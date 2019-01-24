cc6cd74d849262cbf00d73a4092e910284e12d44
Giovanni Delgado <giovanniyatze@gmail.com>
2019-01-23 20:49:27 -0600

    url correction and subcategory modified, added relation with category.

--- engine/urls.py
+++ engine/urls.py
@@ -33,11 +33,12 @@ router.register(r'hashtags', hash_api.Hashtag)
 router.register(r'categories', category_api.Categories)
 router.register(r'subcategories', category_api.Subcategories)
 #router.register(r'multimedia', multimedia_api.MultimediaHandler)
-router.register(r'multimedia-user', multimedia_api.MultimediaUser)
-router.register(r'multimedia-category', multimedia_api.MultimediaCategory)
-router.register(r'multimedia-subcategory', multimedia_api.MultimediaSubcategory)
-router.register(r'multimedia-post', multimedia_api.MultimediaPost)
-#router.register(r'posts', post_api.DataPost)
+router.register(r'multimedia/user', multimedia_api.MultimediaUser)
+router.register(r'multimedia/category', multimedia_api.MultimediaCategory)
+router.register(r'multimedia/subcategory', multimedia_api.MultimediaSubcategory)
+router.register(r'multimedia/post', multimedia_api.MultimediaPost)
+router.register(r'create/posts', post_api.DataPost)
+#router.register(r'create/posts', post_view.Post.as_view())
 
 from engine.multimedia import api_view as multimedia_view
 from engine.post import api_view as post_view
@@ -45,8 +46,8 @@ from engine.post import api_view as post_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/register/', users_api.RegisterUsers.as_view(), name='auth-register'),
-    url(r'^create-multimedia/', multimedia_view.MultimediaHandler.as_view(), name='create-multimedia'),
-    url(r'^create-post/', post_view.Post.as_view(), name='create-post'),
+    #url(r'^create-multimedia/', multimedia_view.MultimediaHandler.as_view(), name='create-multimedia'),
+    #url(r'^create-post/', post_view.Post.as_view(), name='create-post'),
     url(r'^api-token-auth/', obtain_jwt_token, name='create-token'),
     url(r'^api-token-refresh/', refresh_jwt_token, name='refresh-token'),
     url(r'^api-token-verify/', verify_jwt_token, name='verify-token'),
